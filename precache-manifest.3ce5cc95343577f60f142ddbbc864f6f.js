f98692abdc07167150b85e72fc425509ebd8bd63
jenny <jennylee4517@gmail.com>
2019-07-27 13:34:01 +0900

    Updates

--- precache-manifest.3ce5cc95343577f60f142ddbbc864f6f.js
+++ precache-manifest.3ce5cc95343577f60f142ddbbc864f6f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a685dec5143253e375e1c5f5b4c97a2",
+    "revision": "a80cef9656c1af9ccc751ab3fbbd6c66",
     "url": "/react-review/index.html"
   },
   {
-    "revision": "4381b481cd15c96ebeec",
+    "revision": "5742ad8a0fa01b836450",
     "url": "/react-review/static/css/main.06bb5a00.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-review/static/js/2.4e89b4a6.chunk.js"
   },
   {
-    "revision": "4381b481cd15c96ebeec",
-    "url": "/react-review/static/js/main.20fd978f.chunk.js"
+    "revision": "5742ad8a0fa01b836450",
+    "url": "/react-review/static/js/main.865c3629.chunk.js"
   },
   {
     "revision": "7770066a9ae5425a1132",
