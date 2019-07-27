dc90781fe12a21f36582a75b47e1cc02b7d2f546
jenny <jennylee4517@gmail.com>
2019-07-27 13:05:54 +0900

    changed to HashRouter

--- src/root.js
+++ src/root.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+
  
-const Root = () => {
+const Root = ({store}) => {
+
+    
  return (
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
+    // <BrowserRouter>
+    // <App/>
+    // </BrowserRouter>
+
+
+    <Provider store={store} basename={process.env.PUBLIC_URL}> 
+        <HashRouter> 
+            <App/> 
+        </HashRouter> 
+    </Provider>
+
+
  );
 };
 
